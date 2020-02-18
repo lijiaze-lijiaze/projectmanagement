6045b5020843ee1d80e05208a5367348e2740b45
lijiaze <li.jiaze@iwhalecloud.com>
2020-02-18 20:39:12 +0800

    first commit

---
+++ src/trafficlightflashfather.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './App.css';
+
+import { Select } from 'antd';
+
+const { Option } = Select;
+
+class Trafficlightflashfather extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+    
+    }
+   
+  }
+  componentDidMount(){
+   
+  }
+  render () {
+   const{}=this.state
+    return (
+      <div>
+       
+      
+      </div>
+    ) 
+  }
+ 
+}
+
+export default Trafficlightflashfather;
